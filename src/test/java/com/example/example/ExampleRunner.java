92317bb2ea285bc8be08886b451a8acb7ea81452
Pływacz Marcin <mplywacz.ml@inpost.pl>
2023-03-16 13:15:07 +0100

    pom fix

--- src/test/java/com/example/example/ExampleRunner.java
+++ src/test/java/com/example/example/ExampleRunner.java
@@ -1,11 +1,13 @@
 package com.example.example;
 
 import java.util.Collections;
+import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.common.functions.FunctionConfig;
 import org.apache.pulsar.functions.LocalRunner;
 
 class ExampleRunner {
     public static void main(String[] args) throws Exception {
+        System.out.println(Schema.AVRO(Example.class).getSchemaInfo().getSchemaDefinition());
         FunctionConfig functionConfig = new FunctionConfig();
         functionConfig.setName(Function.class.getSimpleName());
         functionConfig.setInputs(Collections.singletonList("persistent://example/example/example"));
